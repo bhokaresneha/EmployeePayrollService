6de5fc7384984319be04be34cdf317d05d2054b1
bhokaresneha <bhokaresneha20@gmail.com>
2022-05-25 08:58:18 +0530

    Added: Storing Employee Payroll Data to a File

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -2,12 +2,10 @@
 package com.bridgelabz;
 //importing classes
 import java.io.*;
-import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import static com.bridgelabz.EmployeePayrollFileIOServices.file;
 
 public class EmployeePayrollService<employeePayrollList> {
     //Creating Scanner Class object to take input from console
@@ -16,14 +14,19 @@ public class EmployeePayrollService<employeePayrollList> {
     //creating ArrayList object
     public static List<EmployeePayroll> employeePayrollList;
 
-    // Constructor of Employee Payroll Service class passing List of Employee Payroll class                           
+    // Constructor of Employee Payroll Service class passing List of Employee Payroll class
     public EmployeePayrollService(List<EmployeePayroll> employeePayrollData) {
         this.employeePayrollList = employeePayrollData;
     }
 
     //Using readEmployeePayrollData() method taking input form User/console.
-    public void readEmployeePayrollData() {
+    public int readEmployeePayrollData(IOService ioService) {
+        if(ioService.equals(IOService.FILE_IO))
+        {
+            EmployeePayrollFileIOServices.readFromFile();
 
+        }else if(ioService.equals(IOService.CONSOLE_IO))
+        {
             System.out.println("Enter Employee Id: ");
             int id = scanner.nextInt();
             System.out.println("Enter Employee Name: ");
@@ -31,30 +34,36 @@ public class EmployeePayrollService<employeePayrollList> {
             System.out.println("Enter Employee Salary: ");
             double salary = scanner.nextDouble();
             employeePayrollList.add(new EmployeePayroll(id, name, salary));
+        }
+        return employeePayrollList.size();
 
     }
 
 
 
     // using writeEmployeePayrollData() method Displaying data
-        private void writeEmployeePayrollData() {
-                System.out.println("\nWriting Employee Payroll Data to console: \n"+employeePayrollList);
+    private void writeEmployeePayrollData(IOService ioService)
+    {
+        if (ioService.equals(IOService.FILE_IO))
+            EmployeePayrollFileIOServices.addToFile(employeePayrollList);
+
+        else
+        System.out.println("\nWriting Employee Payroll Data to console: \n"+employeePayrollList);
 
     }
 
     // main()Method
-    public static void main(String[] args)  {
+    public static void main(String[] args){
         System.out.println("Welcome To Employee Payroll Service");
 
         ArrayList<EmployeePayroll> employeePayroll = new ArrayList<>();
         EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayroll);
         //using EmployeePayrollService class object employeePayrollService calling methods readEmployeePayrollData() and writeEmployeePayrollData
-        employeePayrollService.readEmployeePayrollData();
-        employeePayrollService.writeEmployeePayrollData();
+        int entryCount= employeePayrollService.readEmployeePayrollData(IOService.CONSOLE_IO);
+        System.out.println(entryCount+"Record Added into File");
+        employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
 
 
 
     }
 }
-
-
---
+++ src/main/java/com/bridgelabz/IOService.java
@@ -0,0 +1,5 @@
+package com.bridgelabz;
+
+public enum IOService {
+    CONSOLE_IO, FILE_IO
+}
